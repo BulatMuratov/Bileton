142e3708826a5db884d4d60d531eeaacf7685d0e
 Bulat <muratov_bulatsss@mail.ru>
2026-09-24 01:18:33 +0300

    create events.lifecycle topic

--- kafka/create-topics.sh
+++ kafka/create-topics.sh
@@ -1,10 +1,13 @@
-```bash
 #!/bin/bash
 
 set -e
 
 BOOTSTRAP_SERVERS="kafka-1:19092,kafka-2:19092,kafka-3:19092"
-TOPIC="payments.lifecycle"
+
+TOPICS=(
+  "payments.lifecycle"
+  "events.lifecycle"
+)
 
 echo "Waiting for Kafka..."
 
@@ -17,26 +20,14 @@ done
 
 echo "Kafka is ready."
 
-echo "Creating topic: $TOPIC"
+for TOPIC in "${TOPICS[@]}"; do
+    /opt/kafka/bin/kafka-topics.sh \
+        --bootstrap-server "$BOOTSTRAP_SERVERS" \
+        --create \
+        --if-not-exists \
+        --topic "$TOPIC" \
+        --partitions 3 \
+        --replication-factor 3
+done
 
- /opt/kafka/bin/kafka-topics.sh \
-    --bootstrap-server "$BOOTSTRAP_SERVERS" \
-    --create \
-    --if-not-exists \
-    --topic "$TOPIC" \
-    --partitions 3 \
-    --replication-factor 3
-
-echo "Topic '$TOPIC' is ready."
-
-echo
-echo "Kafka topics:"
-echo
-
-/opt/kafka/bin/kafka-topics.sh \
-    --bootstrap-server "$BOOTSTRAP_SERVERS" \
-    --list
-
-echo
 echo "Kafka topics initialization completed."
-```
